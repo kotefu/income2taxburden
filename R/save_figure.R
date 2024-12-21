da1222f3f1d8965f20e8dde1c027179eb5d23b71
kotefu <52514843+kotefu@users.noreply.github.com>
2024-12-21 22:31:08 +0900

    dpiを変更できるように引数を加えた。

--- R/save_figure.R
+++ R/save_figure.R
@@ -1,13 +1,14 @@
 ## ggplotの図を保存する関数
 library(ggplot2, ragg)
 
-save_figure <- function(filename, plot, width = 180, height = 90) {
+save_figure <- function(filename, plot, width = 180, height = 90, dpi = 300) {
   ggsave(
     filename = here(filename),
     plot = plot,
     device = ragg::agg_png(),
     width = width,
     height = height,
-    units = "mm"
+    units = "mm",
+    dpi = dpi
   )
 }
