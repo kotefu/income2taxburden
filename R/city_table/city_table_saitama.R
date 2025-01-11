81c78444d2ce85c50563c4ba9a47790e3ad4ea26
kotefu <52514843+kotefu@users.noreply.github.com>
2025-01-11 18:12:18 +0900

    さいたま市の国保税率等のtibbleをRコードに外出しした

---
+++ R/city_table/city_table_saitama.R
@@ -0,0 +1,17 @@
+## さいたま市のcity table
+## https://www.city.saitama.lg.jp/001/153/002/004/p010020.html
+
+saitama <- tibble::tibble(
+  med_income_rate = 0.0701,
+  med_eq = 3.5,
+  med_max  = 65,
+  
+  sen_income_rate = 0.026,
+  sen_eq = 1.22,
+  sen_max = 24,
+  
+  nur_income_rate = 0.0224,
+  nur_eq = 1.34,
+  nur_max = 17
+)
+
